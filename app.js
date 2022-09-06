8722c34664234d75adfc8aaaa91a1b7b2506399e
kerndcdx <kervincordoba33@gmail.com>
2022-09-05 21:29:42 -0500

    Subiendo version correcta de la aplicacion

---
+++ app.js
@@ -0,0 +1,23 @@
+const express= require('express');
+const app= express();
+const path= require('path');
+
+app.listen(3031,()=>
+console.log("Servidor encendido")
+);
+
+app.use(express.static('public'));
+
+app.get("/",(req,res)=>
+res.sendFile(path.join(__dirname,"./views/home.html"))
+);
+
+app.get("/home",(req,res)=>
+res.sendFile(path.join(__dirname,"./views/home.html"))
+);
+app.get("/login",(req,res)=>
+res.sendFile(path.join(__dirname,"./views/login.html"))
+);
+app.get("/register",(req,res)=>
+res.sendFile(path.join(__dirname,"./views/register.html"))
+);
